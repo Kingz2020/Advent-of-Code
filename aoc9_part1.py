aa65e6d733ee799a417d52894a9adf61580cbd63
Kingsley Obeng <mystakingz@gmail.com>
2021-02-01 16:07:17 +0100

    Add files via upload

--- aoc9_part1.py
+++ aoc9_part1.py
@@ -3,10 +3,12 @@ import pandas as pd
 
 
 def valid_addends(df, start_idx, preamble, search_idx):
-    """checks if there are two valid addends in the 25(preamble) preceding rows"""
+    """checks if there are two valid addends
+    in the 25(preamble) preceding rows"""
     for index in range(start_idx, start_idx+preamble):
         if (df[df.loc[start_idx:start_idx+24] == df[0][search_idx]
-               - df[0][index]].any()[0]) and (df[0][search_idx] != df[0][index]*2):
+               - df[0][index]].any()[0]) and (df[0][search_idx] !=
+                                              df[0][index]*2):
             return True  # addends exist
     return False
 
@@ -16,7 +18,7 @@ def search_for_index(df):
     for index1, row in df.iterrows():
         if not valid_addends(df, index1, 25, index1+25):
             print(
-                f"the index {index1+25} doesn't have addends, so {df[0][index1+25]}")
+                f"the index answer is {df[0][index1+25]}")
             break
 
 
